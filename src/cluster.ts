b6a436f88a04608de06eeae3421c109094d5f922
Neiman22 <pushkarev_av@inbox.ru>
2023-07-03 17:59:22 +0200

    fix: add cluster.ts file

--- src/cluster.ts
+++ src/cluster.ts
@@ -3,8 +3,17 @@ import os from "os";
 
 const numCPUs = os.cpus().length;
 
-if(cluster.isPrimary) {
-  console.log(`Master ${process.pid} is running`)
+if (cluster.isPrimary) {
+  console.log(`Master ${process.pid} is running`);
+
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`);
+  });
 } else {
-   
+  require('./index');
+  console.log(`Worker ${process.pid} started`);
 }
