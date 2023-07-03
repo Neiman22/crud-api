34e2a0a4e8d1766a94ccec2e6815c4762e2c4fef
Neiman22 <pushkarev_av@inbox.ru>
2023-07-03 20:50:51 +0300

    feat: add gitignore file

--- index.ts
+++ index.ts
@@ -105,4 +105,7 @@ app.use((err: any, req: Request, res: Response) => {
   res.status(500).json({ message: 'Internal server error' });
 });
 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
