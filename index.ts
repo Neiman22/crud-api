4cbaf9c071b00deb8967a7e225267c9fffd871f1
Neiman22 <pushkarev_av@inbox.ru>
2023-07-03 18:19:11 +0200

    fix: add GET api/users/{userId} method

--- index.ts
+++ index.ts
@@ -11,6 +11,7 @@ interface User {
 }
 
 const users: User[] = [];
+const PORT = process.env.PORT || 4000;
 
 function isValidUUID(uuid: string): boolean {
   const uuidRegex =
@@ -21,4 +22,20 @@ function isValidUUID(uuid: string): boolean {
 app.get('/api/users', (req: Request, res: Response) => {
   res.status(200).json(users);
 });
+
+app.get('/api/users/:userId', (req: Request, res: Response) => {
+  const userId = req.params.userId;
+
+  if(!isValidUUID(userId)) {
+    return res.status(400).json({ message: 'Invalid user ID' });
+  }
+
+  const user = users.find((elem) => elem.id === userId);
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  res.status(200).json(user);
+});
+
 
